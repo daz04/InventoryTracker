e39a5fb8cb53d272c07009cb399787d1bb8fdf33
daz04 <danazahreddine@hotmail.com>
2020-11-27 17:54:16 +0200

    Initial Commit

---
+++ InventoryTracker/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  InventoryTracker
+//
+//  Created by Dana Zahreddine on 11/27/20.
+//  Copyright © 2020 Dana Zahreddine. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
